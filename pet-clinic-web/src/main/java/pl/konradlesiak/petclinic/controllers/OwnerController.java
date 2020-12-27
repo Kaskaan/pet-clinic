a3088d8b6bc17a73e90369e82cb6afb2b1573a77
Konrad <kaskaan@gmail.com>
2020-12-27 20:12:01 +0000

    added Owners List to the Owner's HTML template. Updating OwnerController. Closes #17

--- pet-clinic-web/src/main/java/pl/konradlesiak/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/konradlesiak/petclinic/controllers/OwnerController.java
@@ -1,13 +1,22 @@
 package pl.konradlesiak.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.konradlesiak.petclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping({"/owners", "/owners.html"})
-    public String getOwners() {
+    public String getOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/owners";
     }
 }
