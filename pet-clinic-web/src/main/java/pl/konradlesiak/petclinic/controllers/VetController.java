f31178a29477d555f0ca870b53f4e1861a368fef
Konrad <kaskaan@gmail.com>
2020-12-27 21:09:48 +0000

    adding Vet List to Vet HTML Template. Updating Vet Controller

--- pet-clinic-web/src/main/java/pl/konradlesiak/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/konradlesiak/petclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package pl.konradlesiak.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.konradlesiak.petclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"/vets", "vets.html"})
-    public String getVets() {
+    public String getVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/vets";
     }
 }
