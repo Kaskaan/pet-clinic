d35b53f535ebd71e61115b9998ee86fa9af4cf60
Konrad <kaskaan@gmail.com>
2020-12-27 21:37:06 +0000

    adding Pets List inside pets.html. Updating PetController. Closes #25

--- pet-clinic-web/src/main/java/pl/konradlesiak/petclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/pl/konradlesiak/petclinic/controllers/PetController.java
@@ -1,13 +1,22 @@
 package pl.konradlesiak.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.konradlesiak.petclinic.services.PetService;
 
 @Controller
 public class PetController {
 
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
     @GetMapping({"/pets", "/pets.html"})
-    public String getPets() {
+    public String getPets(Model model) {
+        model.addAttribute("pets", petService.findAll());
         return "pets/pets";
     }
 }
