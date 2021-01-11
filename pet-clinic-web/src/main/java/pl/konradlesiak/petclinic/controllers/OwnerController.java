b767ca7539010f9414f4d5f974927149a1be4add
Konrad Lesiak <kaskaan@gmail.com>
2021-01-11 20:17:46 +0000

    adding Owner Details Page. closes #58

--- pet-clinic-web/src/main/java/pl/konradlesiak/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/konradlesiak/petclinic/controllers/OwnerController.java
@@ -3,6 +3,7 @@ package pl.konradlesiak.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import pl.konradlesiak.petclinic.services.OwnerService;
 
 @Controller
@@ -18,5 +19,11 @@ public class OwnerController {
     public String getOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/owners";
+    }
+
+    @GetMapping({"/owners/{id}", "/owners/{id}.html"})
+    public String getOwners(Model model, @PathVariable Long id) {
+        model.addAttribute("owner", ownerService.findById(id));
+        return "owners/owner";
     }
 }
