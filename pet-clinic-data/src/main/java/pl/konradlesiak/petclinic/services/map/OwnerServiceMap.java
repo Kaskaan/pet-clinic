3e44abccfe6fbc12f232f64db963e6a80f15c60b
Konrad Lesiak <kaskaan@gmail.com>
2021-01-11 19:08:47 +0000

    adding possibility to save Pets by Owners. Updating Owner to avoid NullPointerExeption. closes #57

--- pet-clinic-data/src/main/java/pl/konradlesiak/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/pl/konradlesiak/petclinic/services/map/OwnerServiceMap.java
@@ -2,12 +2,23 @@ package pl.konradlesiak.petclinic.services.map;
 
 import org.springframework.stereotype.Service;
 import pl.konradlesiak.petclinic.model.Owner;
+import pl.konradlesiak.petclinic.model.Pet;
 import pl.konradlesiak.petclinic.services.OwnerService;
+import pl.konradlesiak.petclinic.services.PetService;
+import pl.konradlesiak.petclinic.services.PetTypeService;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -20,7 +31,29 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner owner) {
-        return super.save(owner);
+        if(owner != null){
+            if (owner.getPets() != null) {
+                owner.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null){
+                        if(pet.getPetType().getId() == null){
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    } else {
+                        throw new RuntimeException("Pet Type is required");
+                    }
+
+                    if(pet.getId() == null){
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+
+            return super.save(owner);
+
+        } else {
+            return null;
+        }
     }
 
     @Override
