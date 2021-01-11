307a540bbfdff90162f0dfc22de323011aee553c
Konrad Lesiak <kaskaan@gmail.com>
2021-01-11 18:43:46 +0000

    solving "NullPointerException" while building the application. closes #56

--- pet-clinic-data/src/main/java/pl/konradlesiak/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/pl/konradlesiak/petclinic/model/Vet.java
@@ -3,11 +3,12 @@ package pl.konradlesiak.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
 public class Vet extends Person {
 
-    private Set<Speciality> speciality;
+    private Set<Speciality> specialities = new HashSet<>();
 }
