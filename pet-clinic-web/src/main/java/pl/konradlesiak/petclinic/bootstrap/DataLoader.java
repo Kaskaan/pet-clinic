35723196d909b1adccb49b8cce93a3a6b82f86db
Konrad Lesiak <kaskaan@gmail.com>
2021-01-15 13:06:54 +0000

    change setters names inside DataLoader. closes #61

--- pet-clinic-web/src/main/java/pl/konradlesiak/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pl/konradlesiak/petclinic/bootstrap/DataLoader.java
@@ -65,7 +65,7 @@ public class DataLoader implements CommandLineRunner {
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Boglarka");
-        owner1.setSecondName("Palko");
+        owner1.setLastName("Palko");
         owner1.setAddress("17 Best St.");
         owner1.setCity("Katowice");
         owner1.setTelephone("7363466433");
@@ -73,7 +73,7 @@ public class DataLoader implements CommandLineRunner {
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
-        owner2.setSecondName("Glenanne");
+        owner2.setLastName("Glenanne");
         owner2.setAddress("125 Oxford Rd.");
         owner2.setCity("Reading");
         owner2.setTelephone("7672645409");
@@ -81,7 +81,7 @@ public class DataLoader implements CommandLineRunner {
 
         Owner owner3 = new Owner();
         owner3.setFirstName("Andrzej");
-        owner3.setSecondName("Piaseczny");
+        owner3.setLastName("Piaseczny");
         owner3.setAddress("65 Island Rd.");
         owner3.setCity("Reading");
         owner3.setTelephone("7345167345");
@@ -91,13 +91,13 @@ public class DataLoader implements CommandLineRunner {
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
-        vet1.setSecondName("Axe");
+        vet1.setLastName("Axe");
         vet1.getSpecialities().add(savedSpecialityDentistry);
         Vet savedVetSam = vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Roger");
-        vet2.setSecondName("Moore");
+        vet2.setLastName("Moore");
         vet2.getSpecialities().add(savedSpecialitySurgery);
         Vet savedVetRoger = vetService.save(vet2);
 
@@ -105,14 +105,14 @@ public class DataLoader implements CommandLineRunner {
 
         Pet pet1 = new Pet();
         pet1.setPetType(savedPetTypeDog);
-        pet1.setPetName("Rex");
+        pet1.setName("Rex");
         pet1.setBirthDate(LocalDate.now().minusYears(2));
         pet1.setOwner(savedOwnerAndrzej);
         Pet savedPetRex = petService.save(pet1);
 
         Pet pet2 = new Pet();
         pet2.setPetType(savedPetTypeDog);
-        pet2.setPetName("Kutyus");
+        pet2.setName("Kutyus");
         pet2.setBirthDate(LocalDate.now().minusYears(1));
         pet2.setOwner(savedOwnerBoglarka);
         Pet savedPetKutyus = petService.save(pet2);
