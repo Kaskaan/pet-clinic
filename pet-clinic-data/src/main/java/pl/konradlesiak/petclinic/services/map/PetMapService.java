01ed6e618a4365cd8acb62e4318177b33cf22f52
Konrad Lesiak <kaskaan@gmail.com>
2021-01-15 19:35:20 +0000

    updating profiles to use Spring Data Jpa. closes #77

--- pet-clinic-data/src/main/java/pl/konradlesiak/petclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/pl/konradlesiak/petclinic/services/map/PetMapService.java
@@ -1,5 +1,6 @@
 package pl.konradlesiak.petclinic.services.map;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import pl.konradlesiak.petclinic.model.Pet;
 import pl.konradlesiak.petclinic.services.PetService;
@@ -7,6 +8,7 @@ import pl.konradlesiak.petclinic.services.PetService;
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
--- pet-clinic-data/src/main/java/pl/konradlesiak/petclinic/services/map/PetTypeMapService.java
+++ pet-clinic-data/src/main/java/pl/konradlesiak/petclinic/services/map/PetTypeMapService.java
@@ -1,5 +1,6 @@
 package pl.konradlesiak.petclinic.services.map;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import pl.konradlesiak.petclinic.model.PetType;
 import pl.konradlesiak.petclinic.services.PetTypeService;
@@ -7,6 +8,7 @@ import pl.konradlesiak.petclinic.services.PetTypeService;
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
     @Override
     public Set<PetType> findAll() {
