7df4d325583f67fd693e11f3693e9a1e6693313b
Konrad Lesiak <kaskaan@gmail.com>
2021-01-15 18:07:18 +0000

    creating Spring Data JPA Service for Pet Service. closes #48

---
+++ pet-clinic-data/src/main/java/pl/konradlesiak/petclinic/services/springdatajpa/PetJpaService.java
@@ -0,0 +1,48 @@
+package pl.konradlesiak.petclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import pl.konradlesiak.petclinic.model.Pet;
+import pl.konradlesiak.petclinic.repositories.PetRepository;
+import pl.konradlesiak.petclinic.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> petSet = new HashSet<>();
+        petRepository.findAll().forEach(petSet::add);
+        return petSet;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        deleteById(id);
+    }
+}
