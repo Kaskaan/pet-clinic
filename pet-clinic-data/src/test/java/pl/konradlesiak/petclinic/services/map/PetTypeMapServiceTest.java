6879aa6b690bb39216503c6d8e5444d6616da864
Konrad Lesiak <kaskaan@gmail.com>
2021-01-20 12:40:12 +0000

    adding CRUD Tests for PetType Map Service. closes #81

---
+++ pet-clinic-data/src/test/java/pl/konradlesiak/petclinic/services/map/PetTypeMapServiceTest.java
@@ -0,0 +1,58 @@
+package pl.konradlesiak.petclinic.services.map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pl.konradlesiak.petclinic.model.PetType;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetTypeMapServiceTest {
+
+    PetType petType1;
+    PetType petType2;
+
+    PetTypeMapService petTypeMapService;
+
+    @BeforeEach
+    void setUp() {
+        petTypeMapService = new PetTypeMapService();
+
+        petType1 = PetType.builder().name("Dog").build();
+        petTypeMapService.save(petType1);
+
+        petType2 = PetType.builder().name("Cat").build();
+    }
+
+    @Test
+    void findAll() {
+        final Set<PetType> petTypeSet = petTypeMapService.findAll();
+        assertNotNull(petTypeSet);
+        assertEquals(1L, petTypeSet.size());
+    }
+
+    @Test
+    void deleteById() {
+        petTypeMapService.deleteById(petType1.getId());
+        assertFalse(petTypeMapService.findAll().contains(petType1));
+    }
+
+    @Test
+    void delete() {
+        petTypeMapService.delete(petType1);
+        assertFalse(petTypeMapService.findAll().contains(petType1));
+    }
+
+    @Test
+    void save() {
+        petTypeMapService.save(petType2);
+        assertTrue(petTypeMapService.findAll().contains(petType2));
+    }
+
+    @Test
+    void findById() {
+        final PetType petTypeFromService = petTypeMapService.findById(petType1.getId());
+        assertEquals(petType1, petTypeFromService);
+    }
+}
